6c3da802e2f78f1db45b84a2943254917faf2521
kalit_000 <kali.tummala@gmail.com>
2019-06-14 18:45:53 -0400

    new files 2

--- src/main/scala/com/dataframe/extraDFExamples/SparkReadZipFiles.scala
+++ src/main/scala/com/dataframe/extraDFExamples/SparkReadZipFiles.scala
@@ -7,6 +7,7 @@ import java.util.zip.ZipInputStream
 import org.apache.spark.SparkContext
 import org.apache.spark.input.PortableDataStream
 import org.apache.spark.rdd.RDD
+import java.util.zip.ZipFile
 /**
   * Created by kalit_000 on 6/14/19.
   */
@@ -19,19 +20,18 @@ object SparkReadZipFiles {
     def readFile(path: String,
                  minPartitions: Int = sc.defaultMinPartitions): RDD[String] = {
 
-      if (path.endsWith(".zip")) {
+      if (path.toLowerCase.contains("zip")) {
+
         sc.binaryFiles(path, minPartitions)
-          .flatMap { case (name: String, content: PortableDataStream) =>
-            val zis = new ZipInputStream(content.open)
-            Stream.continually(zis.getNextEntry)
-              .takeWhile {
-                case null => zis.close(); false
-                case _ => true
-              }
-              .flatMap { _ =>
-                val br = new BufferedReader(new InputStreamReader(zis))
-                Stream.continually(br.readLine()).takeWhile(_ != null).map(x => x.toString+"~"+name)
-              }
+          .flatMap {
+            case (zipFilePath, zipContent) ⇒
+              val zipInputStream = new ZipInputStream(zipContent.open())
+              Stream.continually(zipInputStream.getNextEntry)
+                .takeWhile(_ != null)
+                .map { x ⇒
+                  val filename1 = x.getName
+                  scala.io.Source.fromInputStream(zipInputStream, "UTF-8").getLines.mkString(s"~${filename1}\n")+s"~${filename1}"
+                } #::: { zipInputStream.close; Stream.empty[String] }
           }
       } else {
         sc.textFile(path, minPartitions)
@@ -50,16 +50,13 @@ object SparkReadZipFiles {
 
     import spark.implicits._
 
-
-    val rdd = sc.readFile("sparkZipDataSets/Archive.zip")
-
+    val rdd = sc.readFile("sparkZipDataSets/file_2.zip")
 
     val df = rdd.toDF("data")
 
     df.createOrReplaceTempView("table")
 
     spark.sql("select split(data,'~')[0] as data_values,split(data,'~')[1] as file_name from table").show(false)
-
 
     spark.stop()
 
