9672855e56cc40740da5049e063ba575c94cea8f
sriharitummala <kali.tummala@gmail.com>
2020-02-07 10:34:47 -0600

    Python Config Praser not to lower

--- src/main/scala/com/datafram/part39/PySpark/PySparkConfFromFiles.py
+++ src/main/scala/com/datafram/part39/PySpark/PySparkConfFromFiles.py
@@ -1,7 +1,13 @@
 genericconfig = configParser.RawConfigParser()
 genericconfig.read("configFile")
 
+self.config = ConfigParser.ConfigParser()
+#important line for not lowering
+# https://stackoverflow.com/questions/19359556/configparser-reads-capital-keys-and-make-them-lower-case
+self.config.optionxform = str
+
 genericSparkConfig = genericconfig.items("spark_properties")
+
 
 sc_conf = SparkConf()
 configObj = sc_conf.setAppName(job_name).setAll(genericSparkConfig)
